f274a7af317f681c9b50d4e235979f76ca9094bb
Haamid Raza Kazi <haamidraza03@gmail.com>
2025-01-23 00:08:28 +0530

    backend done

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+function check()
+{
+    let Name = document.getElementById('name').value;
+    let Email = document.getElementById('email').value;
+    let Query = document.getElementById('query').value;
 
-// Show or hide the button based on scroll position
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 10) { // Show the button after scrolling 200px
-        scrollToTopBtn.style.display = 'block';
-    } else {
-        scrollToTopBtn.style.display = 'none';
+    let user={
+        name:Name,
+        email:Email,
+        query:Query
     }
-});
 
-// Scroll to the top smoothly when the button is clicked
-scrollToTopBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
+    if(Name.length>1 && Email.length>10 && Query.length>1)
+    {
+        fetch('https://portfolio-a5c8d-default-rtdb.firebaseio.com/user.json',
+        {
+            method:'post',
+            body:JSON.stringify(user)
+        }).then(()=>
+        {
+            alert('Thank You For Contacting.');
+            document.getElementById('name').value="";
+            document.getElementById('email').value="";
+            document.getElementById('query').value="";
+            window.location.href='./index.html';
+        }) 
+    }
+    else if(Name.length<1 && Email.length<10 && Query.length<1){
+        alert('Please fill all Details.')
+    }
+    else if(Name.length<=1)
+    {
+        alert('Please give a valid Name.')
+    }
+    else if(Query.length<=1){
+        alert('Your message is too short.')
+    }
+}
